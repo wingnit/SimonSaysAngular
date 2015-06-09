6f9b72710781f2908ffbce8aed3b2337524318d3
James Barnes <jay.barnes88@gmail.com>
2015-06-08 22:33:49 -0400

    switch to angular layout

---
+++ app/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Declare app level module which depends on views, and components
+angular.module('myApp', [
+    'ngRoute',
+    'myApp.view1',
+    'myApp.view2',
+    'myApp.version'
+]).
+    config(['$routeProvider', function ($routeProvider) {
+        $routeProvider.otherwise({redirectTo: '/view1'});
+    }]).
+    controller('index', function ($scope) {
+        $scope.hello = 'hello';
+
+    });
---
+++ app/index-controller.js
@@ -0,0 +1,7 @@
+'use strict';
+
+angular.module('simonSays.index').controller('index', function($scope) {
+    $scope.hello = 'hello';
+
+
+});
